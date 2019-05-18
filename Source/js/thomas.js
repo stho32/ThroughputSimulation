7a4048f40f376f827884e5bdb2044b3f6bbf2aaa
Stefan Hoffmann <sthoffmann@onlinehome.de>
2019-05-18 14:11:44 +0200

    first draft

---
+++ Source/js/thomas.js
@@ -0,0 +1,44 @@
+﻿function Node(label) {
+    var publicApi = {};
+
+    publicApi.label = label;
+    publicApi.incomingEdges = [];
+    publicApi.outgoingEdges = [];
+
+    publicApi.connectIncomingEdge = (edge, fromNode) => {
+        publicApi.incomingEdges.push({
+            edge : edge, 
+            fromNode : fromNode
+        });
+    }
+
+    publicApi.connectOutgoingEdge = (edge, toNode) => {
+        publicApi.outgoingEdges.push({
+            edge : edge, 
+            toNode : toNode
+        });
+    }
+
+    return publicApi;
+}
+
+function Edge(label) {
+    var publicApi = {};
+
+    publicApi.label = label;
+
+    publicApi.connect = (fromNode, toNode) => {
+        fromNode.connectOutgoingEdge(publicApi);
+        toNode.connectIncomingEdge(publicApi);
+    }
+
+    return publicApi;
+}
+
+var a = Node("A");
+var b = Node("B");
+var edge = Edge(3).connect(a,b);
+ 
+var sampleGraph = [
+    a,b,edge
+];
